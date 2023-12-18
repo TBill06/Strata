2e9871d08f543e63ccaaf9f58921094e154f465e
TBill06 <bill.tushar2@gmail.com>
2023-12-18 14:08:52 -0330

    Day4 Done

---
+++ day4_ScratchCards/day4.py
@@ -0,0 +1,44 @@
+# Advent of Code Day 4
+
+# Reading input from file
+with open("input.txt", "r") as file:
+    lines = file.read().splitlines()
+    splitfunc = lambda x,num: x.split(":")[1].split("|")[num].strip()
+    book = {splitfunc(i,0) : splitfunc(i,1) for i in lines}
+
+# Part 1
+ans = 0
+for i in book:
+    count = 0
+    val = 0
+    nums = book[i].split(" ")
+    win_nums = i.split(" ")
+    for j in nums:
+        if j.isdigit() and j in win_nums:
+            count += 1
+    for i in range(count):
+        if i == 0:
+            val += 1
+        else:
+            val *= 2
+    ans += val
+print(ans)
+
+#Part 2
+def win(i):
+    count = 0
+    nums = book[i].split(" ")
+    win_nums = i.split(" ")
+    for j in nums:
+        if j.isdigit() and j in win_nums:
+            count += 1
+    return count
+
+nums_arr = [1]*len(book)
+for index, val in enumerate(book):
+    print("Card:",index+1)
+    for j in range(nums_arr[index]):
+        count = win(val)
+        if count:
+            nums_arr[index+1:index+count+1] = [x+1 for x in nums_arr[index+1:index+count+1]]
+print(sum(nums_arr))
