2324a43cde110693bf966d80b5d0743d48a25448
TBill06 <bill.tushar2@gmail.com>
2025-05-20 10:45:04 -0230

    11/100

--- 11.pangram.ts
+++ 11.pangram.ts
@@ -8,3 +8,11 @@ export function isPangram(input: string): boolean {
     }
     return true;
 }
+
+// Use of regex and Set to check for unique characters and their count.
+// Set gives O(1) time complexity.
+// export function isPangram(val: string): boolean {
+//   const inputVal = val.toLowerCase().match(/[a-z]/g);
+//   const uni = new Set(inputVal);
+//   return uni.size === 26;
+// }
