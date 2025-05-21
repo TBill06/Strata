9d56a10176edad246308a487e52a5598c5f3b116
TBill06 <bill.tushar2@gmail.com>
2025-05-21 17:17:00 -0230

    14/100 v2.0

--- 14.robot-name.ts
+++ 14.robot-name.ts
@@ -34,3 +34,64 @@ export class Robot {
     Robot.usedNames.clear();
   }
 }
+
+// To pass the last test, v2.0
+// export class Robot {
+//   #nameVal: string;
+//   static usedNames = new Set<string>();
+//   static namePool: string[] = [];
+  
+//   constructor() {
+//     if (Robot.namePool.length === 0) {
+//       this.#generateNamePool();
+//     }
+//     this.#nameVal = this.#getName();
+//     Robot.usedNames.add(this.#nameVal);
+//   }
+
+//   #generateNamePool(): void {
+//     if (Robot.namePool.length === 0) {
+//       for (let i = 65; i <= 90; i++) {
+//         for (let j = 65; j <= 90; j++) {
+//           for (let d1 = 0; d1 <= 9; d1++) {
+//             for (let d2 = 0; d2 <= 9; d2++) {
+//               for (let d3 = 0; d3 <= 9; d3++) {
+//                 const name = String.fromCharCode(i) + 
+//                              String.fromCharCode(j) + 
+//                              d1 + d2 + d3;
+//                 Robot.namePool.push(name);
+//               }
+//             }
+//           }
+//         }
+//       }
+//       this.#shuffleArray(Robot.namePool);
+//     }
+//   }
+
+//   #shuffleArray(array: string[]): void {
+//     for (let i = array.length - 1; i > 0; i--) {
+//       const j = Math.floor(Math.random() * (i + 1));
+//       [array[i], array[j]] = [array[j], array[i]];
+//     }
+//   }
+
+//   #getName(): string {
+//     return Robot.namePool.pop()!;
+//   }
+
+//   public get name(): string {
+//     return this.#nameVal;
+//   }
+
+//   public resetName(): void {
+//     Robot.usedNames.delete(this.#nameVal);
+//     this.#nameVal = this.#getName();
+//     Robot.usedNames.add(this.#nameVal);
+//   }
+
+//   public static releaseNames(): void {
+//     Robot.usedNames.clear();
+//     Robot.namePool = [];
+//   }
+// }
