15bd72ee84af2b8fd5761da35bec8b5fe31afab4
TBill06 <bill.tushar2@gmail.com>
2025-05-29 01:12:37 -0230

    19/100 v2.0

--- 19.difference-of-squares.ts
+++ 19.difference-of-squares.ts
@@ -1,26 +1,44 @@
 export class Squares {
-  private num: number;
-  constructor(count: number) {
-    this.num = count;
-  }
+  constructor(private num: number) {}
 
   get sumOfSquares(): number {
-    let sum: number = 0;
-    for (let i = 1; i <= this.num; i++) {
-      sum += i**2;
-    }
-    return sum;
+    return (this.num * (this.num + 1) * (2 * this.num + 1)) / 6;
   }
 
   get squareOfSum(): number {
-    let sum: number = 0;
-    for (let i = 1; i <= this.num; i++) {
-      sum += i;
-    }
-    return sum**2;
+    const sum = (this.num * (this.num + 1)) / 2;
+    return sum * sum;
   }
 
   get difference(): number {
     return this.squareOfSum - this.sumOfSquares;
   }
 }
+
+// v1.0 old stuff
+// export class Squares {
+//   private num: number;
+//   constructor(count: number) {
+//     this.num = count;
+//   }
+
+//   get sumOfSquares(): number {
+//     let sum: number = 0;
+//     for (let i = 1; i <= this.num; i++) {
+//       sum += i**2;
+//     }
+//     return sum;
+//   }
+
+//   get squareOfSum(): number {
+//     let sum: number = 0;
+//     for (let i = 1; i <= this.num; i++) {
+//       sum += i;
+//     }
+//     return sum**2;
+//   }
+
+//   get difference(): number {
+//     return this.squareOfSum - this.sumOfSquares;
+//   }
+// }
