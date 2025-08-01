9dbf69ee84671dbe5de27d3d2fc4e41947cb2e13
TBill06 <bill.tushar2@gmail.com>
2025-08-01 11:43:07 -0230

    31/100 done

--- 31.hamming.ts
+++ 31.hamming.ts
@@ -13,3 +13,9 @@ export function computev2(left: string, right: string): number {
   if (left.length !== right.length) throw new Error("DNA strands must be of equal length.");
   return left.split('').reduce((acc, curr, idx) => acc + (curr !== right[idx] ? 1 : 0), 0);
 }
+
+// v3.0 Filter and Length
+export function computev3(left: string, right: string): number {
+  if (left.length !== right.length) throw new Error("DNA strands must be of equal length.");
+  return left.split('').filter((char, idx) => char !== right[idx]).length;
+}
