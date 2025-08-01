490cd87d0dd6a2f92fe8897aaae2485d26406f34
TBill06 <bill.tushar2@gmail.com>
2025-08-01 11:42:50 -0230

    31/100 v2

---
+++ 31.hamming.ts
@@ -0,0 +1,15 @@
+// v1.0 Easy peasy
+export function compute(left: string, right: string): number {
+  let hamming: number = 0;
+  if (left.length != right.length) throw new Error ("DNA strands must be of equal length.")
+  for (let i=0; i<left.length; i++) {
+    if(left[i] != right[i]) hamming++
+  }
+  return hamming;
+}
+
+// v2.0 One-liner
+export function computev2(left: string, right: string): number {
+  if (left.length !== right.length) throw new Error("DNA strands must be of equal length.");
+  return left.split('').reduce((acc, curr, idx) => acc + (curr !== right[idx] ? 1 : 0), 0);
+}
